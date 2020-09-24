10e7db0955602f8768559ab3b61deb5a04aa0fdf
Valentine Wu <jingwen3@andrew.cmu.edu>
2020-09-24 13:18:50 -0400

    Draft client, server and LFD class; Able to run client-server echo application; LFD is able to detect whether server is dead or not

---
+++ src/Client.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Client {
+    private final String hostname;
+    private final int port;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String name;
+
+
+    public Client (String hostname, int port, String name) {
+        this.hostname = hostname;
+        this.port = port;
+        this.name = name;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Client client = new Client("localhost", 8818, args[0]);
+        client.connect();
+        client.chat();
+    }
+
+    private void connect() {
+        try {
+            this.socket = new Socket(this.hostname, this.port);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+   private void chat() throws IOException {
+        String line = "";
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        while ( (line = stdIn.readLine()) != null) {
+            if (line.equals("quit")) {
+                break;
+            }
+            // send request to the server and then prints it in console
+            sendRequest(line);
+            // print the reply from server
+            receiveReply();
+        }
+    }
+
+    private void sendRequest(String line) {
+        out.println(this.name + " " +line);
+        printTimestamp();
+        System.out.printf("Sent <%s, S1, request> %s %n", this.name, line);
+    }
+    private void receiveReply() throws IOException {
+        printTimestamp();
+        System.out.printf("Received <%s, S1, reply> %s %n", this.name, in.readLine());
+    }
+
+    private void printTimestamp() {
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        System.out.printf("[ %s ] ", timeStamp);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Server extends Thread{
+    private final static int serverPort = 8818;
+    public static int state;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            while (true) {
+                // waits for client to connect
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepting client connection...");
+                ServerHandler handler = new ServerHandler(clientSocket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ServerHandler extends Thread {
+        private final Socket clientSocket;
+        private OutputStream out;
+        private BufferedReader in;
+
+
+        public ServerHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                acceptClient();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void acceptClient() throws IOException {
+            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            this.out = clientSocket.getOutputStream();
+            String line;
+            while ((line = in.readLine()) != null) {
+                // all client messages are in format "clientname message"
+                // retrieve the client name by splitting the line
+                String[] tokens = line.split(" ", 2);
+                if (tokens != null && tokens.length > 0) {
+                    String client = tokens[0];
+                    String msg = tokens[1];
+
+                    if (client.contains("LFD")) {
+                        heartbeat(client);
+                    } else {
+                        receiveRequest(client, msg);
+                        printState();
+                        sendReply(client, msg);
+                    }
+                }
+            }
+
+        }
+
+        private void heartbeat(String LFD) throws IOException {
+            printTimestamp();
+            System.out.printf("S1 receives heartbeat from %s %n", LFD);
+            printTimestamp();
+            System.out.printf("S1 sending heartbeat to %s %n", LFD);
+            String reply = "heartbeat\n";
+            out.write(reply.getBytes());
+        }
+
+        private void printState() {
+            printTimestamp();
+            System.out.println("my_state_S1 = " + (++state));
+        }
+
+        private void receiveRequest(String client, String msg) {
+            printTimestamp();
+            System.out.printf("Receiving <%s, S1, request> %s %n", client, msg);
+        }
+        private void sendReply(String client, String msg) throws IOException {
+            printTimestamp();
+            System.out.printf("Sending <%s, S1, reply> %s %n", client, msg);
+            msg += "\n";
+            out.write(msg.getBytes());
+        }
+
+        private void printTimestamp() {
+            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+            System.out.printf("[ %s ] ", timeStamp);
+        }
+    }
+}
